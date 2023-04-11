176cae6cd1ebc5620e043f4c76ff7c549b71433a
spide <spider322@naver.com>
2023-04-12 02:52:12 +0900

    modifying code

--- calc.py
+++ calc.py
@@ -5,21 +5,21 @@ def plus(x, y):
 
 # main function
 def main():
-    check = 1
     print("Welcome to calcuator")
-    while check >= 1:        
+    while True:        
         print("0: exit, 1: plus")
-        check = int(input())
-        if check == 1:
+        check = input()
+        if check == "1":
             print("First Number")
             x = int(input())
             print("Second Number")
             y = int(input())
             print("answer : ", plus(x,y))
-        elif check > 1:
-            print("Unsupported")
-        else:
+        elif check == "0":
             print("Thank you")
+	    break
+        else:
+            print("Please enter number")
 
 if __name__ == "__main__":
     main()
